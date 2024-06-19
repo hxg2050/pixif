355a46056be3bbad8bed95564515a236f44d545b
youxia <675567585@qq.com>
2024-06-19 20:45:02 +0700

    多行文本输入框

--- src/ui/Input.ts
+++ src/ui/Input.ts
@@ -1,15 +1,25 @@
-import { StrokeStyle, Ticker } from "pixi.js";
+import { StrokeStyle, Ticker, Graphics as PIXIGraphics } from "pixi.js";
 import { GameObject, Graphics, Group, Label, LabelStyle } from "../core";
 
 export class Input extends Group {
-
+    /**
+     * 背景样式
+     */
     public graphics!: Graphics;
-    public element!: HTMLTextAreaElement;
+    /**
+     * 使用html标签进行输入处理
+     */
+    public element!: HTMLTextAreaElement | HTMLInputElement;
+    /**
+     * 输入框失去焦点时显示的内容
+     */
     public value!: Label;
+
+    private _mask!: PIXIGraphics;
 
     set width(val: number) {
         super.width = val;
-        this.resize();
+        this._isResize = true;
     }
     get width() {
         return super.width;
@@ -17,7 +27,7 @@ export class Input extends Group {
 
     set height(val: number) {
         super.height = val;
-        this.resize();
+        this._isResize = true;
     }
     get height() {
         return super.height
@@ -25,7 +35,7 @@ export class Input extends Group {
 
     set x(val: number) {
         super.x = val;
-        this.updateTransform();
+        this._isUpdateTransform = true;
     }
     get x() {
         return super.x;
@@ -33,7 +43,7 @@ export class Input extends Group {
 
     set y(val: number) {
         super.y = val;
-        this.updateTransform();
+        this._isUpdateTransform = true;
     }
     get y() {
         return super.y
@@ -41,7 +51,7 @@ export class Input extends Group {
 
     set scaleX(val: number) {
         super.scaleX = val;
-        this.updateTransform();
+        this._isUpdateTransform = true;
     }
     get scaleX() {
         return super.scaleX
@@ -49,54 +59,99 @@ export class Input extends Group {
 
     set scaleY(val: number) {
         super.scaleY = val;
-        this.updateTransform();
+        this._isUpdateTransform = true;
     }
     get scaleY() {
         return super.scaleY
     }
-
-    private _isUpdateTransform = false;
-    updateTransform() {
-        this._isUpdateTransform = true;
+    
+    /**
+     * 更新mask，条件：1、padding改变 2、宽高改变
+     */
+    protected updateMask() {
+        this._mask.height = this.height - this.paddingTop;
+        this._mask.width = this.width - this.paddingLeft;
     }
 
-    private _isResize = false;
-    resize(): void {
-        this._isResize = true;
-        this._isDrawStyle = true;
+    protected _isUpdateElementSize = false;
+    protected updateElementSize() {
+        this._isUpdateElementSize = false;
+        this.element.style.width = this.width + 'px'
+        this.element.style.height = this.height + 'px'
+        this.element.style.borderWidth = this._padding.join('px ') + 'px'
+        this.element.style.fontSize = this.fontSize + 'px'
+        this.updateMask();
+    }
+
+    private _isUpdateTransform = false;
+    /**
+     * 更新2d变换，位置，缩放，斜切，旋转
+     */
+    updateTransform() {
+        this._isUpdateTransform = false;
+        const { a, b, c, d, tx, ty } = this.display.worldTransform;
+        this.element.style.transform = `matrix(${a}, ${b}, ${c}, ${d}, ${tx}, ${ty})`
+        this.element.style.transformOrigin = `${this.anchorX}% ${this.anchorY}%`;
+        this.updateMask();
+    }
+
+    protected _isResize = false;
+    /**
+     * 更新大小
+     */
+    public resize(): void {
+        this._isResize = false;
+        this._isUpdateElementSize = true;
+        this.value.x = this.paddingLeft;
+        this.value.y = this.paddingTop;
+        this._labelStyle.lineHeight = this.height;
+        this.updateMask();
+    }
+
+    protected createElement() {
+        this.element = document.createElement('input')
     }
 
     public async render() {
-        this.element = document.createElement('textarea')
+        const mask = new PIXIGraphics();
+        mask.rect(0, 0, 100, 50)
+        mask.fill(0x0000ff);
+        this.display.addChild(mask);
+        this._mask = mask;
+        this.createElement();
         this.element.className = 'textarea'
         this.element.style.display = 'none';
         this.element.onfocus = this.onFocus.bind(this);
         this.element.onblur = this.onBlur.bind(this);
+        
+        this.fontFamily = this._labelStyle.fontFamily;
+        this.fontSize = this._labelStyle.fontSize;
+
         window.document.body.append(this.element);
         this.graphics = await GameObject.instantiate(Graphics, this)
         this.value = await GameObject.instantiate(Label, this, {
-            style: this._labelStyle
+            style: this._labelStyle,
+            x: this.paddingLeft
         })
+        this.value.display.mask = mask;
 
 
         this.graphics.display.eventMode = 'static';
         this.graphics.display.on('pointerdown', this.focus, this)
     }
 
-    public update(dt: number): void {
+    public update(): void {
         if (this._isUpdateTransform) {
-            this._isUpdateTransform = false;
-            const { a, b, c, d, tx, ty } = this.display.worldTransform;
-            this.element.style.transform = `matrix(${a}, ${b}, ${c}, ${d}, ${tx}, ${ty})`
-            this.element.style.transformOrigin = `${this.anchorX}% ${this.anchorY}%`;
+            this.updateTransform();
         }
         if (this._isResize) {
-            this._isResize = false;
-            this.element.style.width = this.width + 'px'
-            this.element.style.height = this.height + 'px'
+            this.resize();
         }
         if (this._isDrawStyle) {
             this.drawStyle();
+        }
+        if (this._isUpdateElementSize) {
+            this.updateElementSize();
         }
     }
 
@@ -118,29 +173,82 @@ export class Input extends Group {
         this._isDrawStyle = true;
     }
 
-    private _labelStyle = new LabelStyle({
-        fontSize: 16
+    protected _labelStyle = new LabelStyle({
+        fontSize: 16,
+        lineHeight: 16
     })
+    set fontFamily(val: string | string[]) {
+        this._labelStyle.fontFamily = val;
+        this.element.style.fontFamily = Array.isArray(val) ? val.join(',') : val;
+    }
+    get fontFamily() {
+        return this._labelStyle.fontFamily;
+    }
+
     set fontSize(val: number) {
         this._labelStyle.fontSize = val;
+        this.element.style.fontSize = val + 'px';
     }
     get fontSize() {
         return this._labelStyle.fontSize;
     }
 
-    private _isDrawStyle = false;
+    private _isDrawStyle = true;
+    /**
+     * 绘制背景样式
+     */
     public drawStyle() {
+        this._isDrawStyle = false;
         this.graphics.clear();
         this.graphics.rect(0, 0, this.width, this.height);
         this.graphics.fill(this._backgroundColor)
         this.graphics.stroke(this._stokeStyle);
     }
 
+    protected _padding = [0, 6, 0, 6];
+    set paddingLeft(val: number) {
+        this._padding[3] = val;
+        this._isResize = true;
+        this._isUpdateElementSize = true;
+    }
+    get paddingLeft() {
+        return this._padding[3];
+    }
+
+    set paddingRight(val: number) {
+        this._padding[1] = val;
+        this._isResize = true;
+        this._isUpdateElementSize = true;
+    }
+    get paddinRight() {
+        return this._padding[1];
+    }
+
+    set paddingTop(val: number) {
+        this._padding[0] = val;
+        this._isResize = true;
+        this._isUpdateElementSize = true;
+    }
+    get paddingTop() {
+        return this._padding[0]
+    }
+    
+    set paddingBottom(val: number) {
+        this._padding[2] = val;
+        this._isResize = true;
+        this._isUpdateElementSize = true;
+    }
+    get paddingBottom() {
+        return this._padding[2]
+    }
+    
+
     private onFocus() {
         console.log('focus');
         this.value.display.visible = false
         this.borderColor = 0x0000ff;
         this.borderSize = 2
+        this.emitter.emit('focus');
     }
     public focus() {
         console.log('f');
@@ -157,6 +265,7 @@ export class Input extends Group {
         this.element.style.display = 'none';
         this.value.value = this.element.value;
         this.value.display.visible = true
+        this.emitter.emit('blur');
     }
     public blur() {
         this.element.blur()
---
+++ src/ui/Textarea.ts
@@ -0,0 +1,33 @@
+import { Input } from "./Input";
+
+export class Textarea extends Input {
+
+    set lineHeight(val: number) {
+        this._labelStyle.lineHeight = val;
+        this.element.style.lineHeight = val + 'px';
+    }
+    get lineHeight() {
+        return this._labelStyle.lineHeight;
+    }
+
+    protected _padding = [6, 6, 6, 6];
+    protected createElement(): void {
+        this.element = document.createElement('textarea')
+    }
+
+    public async render() {
+        super.render()
+        this.lineHeight = this._labelStyle.lineHeight;
+        this._labelStyle.wordWrap = true;
+        this._labelStyle.breakWords = true;
+    }
+
+    public resize(): void {
+        this._isResize = false;
+        this._isUpdateElementSize = true;
+        this.value.x = this.paddingLeft;
+        this.value.y = this.paddingTop;
+        this._labelStyle.wordWrapWidth = this.width - this.paddingLeft - this.paddinRight;
+        this.updateMask();
+    }
+}
