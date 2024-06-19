6ddf23794a8252fe67acb70d8baabb7468631030
youxia <675567585@qq.com>
2024-06-19 13:58:33 +0700

    完善Input控件

--- src/core/graphics/index.ts
+++ src/core/graphics/index.ts
@@ -1,5 +1,5 @@
 import { GameObject } from "../GameObject";
-import {ColorSource, Graphics as PIXIGraphics} from 'pixi.js'
+import {ColorSource, Graphics as PIXIGraphics, StrokeInput} from 'pixi.js'
 
 export class Graphics extends GameObject<PIXIGraphics> {
     public display = new PIXIGraphics();
@@ -8,28 +8,38 @@ export class Graphics extends GameObject<PIXIGraphics> {
 
     }
 
-    public beginFill(color: ColorSource = 0, alpha?: number) {
-        this.display.beginFill(color, alpha);
+    // public beginFill(color: ColorSource = 0, alpha?: number) {
+    //     this.display.beginFill(color, alpha);
+    //     return this;
+    // }
+
+    // public endFill() {
+    //     this.display.endFill();
+    //     return this;
+    // }
+
+    public rect(x: number, y: number, width: number, height: number) {
+        this.display.rect(x, y, width, height);
         return this;
     }
 
-    public endFill() {
-        this.display.endFill();
+    public roundRect(x: number, y: number, width: number, height: number, radius: number) {
+        this.display.roundRect(x, y, width, height, radius);
         return this;
     }
 
-    public drawRect(x: number, y: number, width: number, height: number) {
-        this.display.drawRect(x, y, width, height);
+    public circle(x: number, y: number, radius: number) {
+        this.display.circle(x, y, radius);
         return this;
     }
 
-    public drawRoundedRect(x: number, y: number, width: number, height: number, radius: number) {
-        this.display.drawRoundedRect(x, y, width, height, radius);
+    public stroke(style?: StrokeInput) {
+        this.display.stroke(style)
         return this;
     }
 
-    public drawCircle(x: number, y: number, radius: number) {
-        this.display.drawCircle(x, y, radius);
+    public fill(style?: StrokeInput) {
+        this.display.fill(style)
         return this;
     }
 
--- src/ui/Input.ts
+++ src/ui/Input.ts
@@ -1,8 +1,11 @@
-import { GameObject, Group } from "../core";
+import { StrokeStyle, Ticker } from "pixi.js";
+import { GameObject, Graphics, Group, Label, LabelStyle } from "../core";
 
 export class Input extends Group {
 
+    public graphics!: Graphics;
     public element!: HTMLTextAreaElement;
+    public value!: Label;
 
     set width(val: number) {
         super.width = val;
@@ -55,28 +58,35 @@ export class Input extends Group {
     private _isUpdateTransform = false;
     updateTransform() {
         this._isUpdateTransform = true;
-        //  `translate(${this.x}px, ${this.y}px) scale(${this.scaleX}, ${this.scaleY}) rotate(${this.rotation}deg)`
-        // console.log(this.display.worldTransform, this.display.localTransform, this.display.groupTransform, this.display)
     }
 
     private _isResize = false;
     resize(): void {
         this._isResize = true;
-        // this.element.style.width = this.width + 'px'
-        // this.element.style.height = this.height + 'px'
-        // console.log(this.width, this.height);
+        this._isDrawStyle = true;
     }
 
     public async render() {
         this.element = document.createElement('textarea')
         this.element.className = 'textarea'
+        this.element.style.display = 'none';
+        this.element.onfocus = this.onFocus.bind(this);
+        this.element.onblur = this.onBlur.bind(this);
         window.document.body.append(this.element);
+        this.graphics = await GameObject.instantiate(Graphics, this)
+        this.value = await GameObject.instantiate(Label, this, {
+            style: this._labelStyle
+        })
+
+
+        this.graphics.display.eventMode = 'static';
+        this.graphics.display.on('pointerdown', this.focus, this)
     }
 
     public update(dt: number): void {
         if (this._isUpdateTransform) {
             this._isUpdateTransform = false;
-            const {a, b, c, d, tx, ty} = this.display.worldTransform;
+            const { a, b, c, d, tx, ty } = this.display.worldTransform;
             this.element.style.transform = `matrix(${a}, ${b}, ${c}, ${d}, ${tx}, ${ty})`
             this.element.style.transformOrigin = `${this.anchorX}% ${this.anchorY}%`;
         }
@@ -85,5 +95,70 @@ export class Input extends Group {
             this.element.style.width = this.width + 'px'
             this.element.style.height = this.height + 'px'
         }
+        if (this._isDrawStyle) {
+            this.drawStyle();
+        }
+    }
+
+    private _stokeStyle: StrokeStyle = { width: 1, color: 0x828282 }
+
+    private _backgroundColor = 0xffffff;
+    set backgroundColor(val: number) {
+        this._backgroundColor = val
+        this._isDrawStyle = true;
+    }
+
+    set borderColor(val: number) {
+        this._stokeStyle.color = val;
+        this._isDrawStyle = true;
+    }
+
+    set borderSize(val: number) {
+        this._stokeStyle.width = val;
+        this._isDrawStyle = true;
+    }
+
+    private _labelStyle = new LabelStyle({
+        fontSize: 16
+    })
+    set fontSize(val: number) {
+        this._labelStyle.fontSize = val;
+    }
+    get fontSize() {
+        return this._labelStyle.fontSize;
+    }
+
+    private _isDrawStyle = false;
+    public drawStyle() {
+        this.graphics.clear();
+        this.graphics.rect(0, 0, this.width, this.height);
+        this.graphics.fill(this._backgroundColor)
+        this.graphics.stroke(this._stokeStyle);
+    }
+
+    private onFocus() {
+        console.log('focus');
+        this.value.display.visible = false
+        this.borderColor = 0x0000ff;
+        this.borderSize = 2
+    }
+    public focus() {
+        console.log('f');
+        this.element.style.display = 'block';
+        Ticker.shared.addOnce(() => {
+            this.element.focus()
+        })
+    }
+
+    private onBlur() {
+        console.log('blur');
+        this.borderColor = 0x828282;
+        this.borderSize = 1
+        this.element.style.display = 'none';
+        this.value.value = this.element.value;
+        this.value.display.visible = true
+    }
+    public blur() {
+        this.element.blur()
     }
 }
